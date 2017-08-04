a94f74c9f94b5a300df508bbe429948ef6505977
Shubham Mahajan <imahajanshubham@gmail.com>
2017-08-05 01:38:27 +0530

    My first attempt on C++ :)

--- Aug-Dec 2017/timeCalc.c
+++ Aug-Dec 2017/timeCalc.c
@@ -1,1 +1,62 @@
+# include <iostream>
 
+
+using namespace std;
+
+
+/*
+ *      Calculating Total Time of Two Different Class Objects using C++.
+ *                               ----------
+ *          
+ *          Core Logics - 1. Values are assigned to 'data members' using Class Constructors.
+ *                        2. In Case of No Value, Default Values are used.
+ *                        3. A 'friend function' that processes/adds the Time of both the Objects.
+ *                        4. Finnaly, print the Values of the third Object.
+ */
+
+
+class timeCalc {
+
+    int hours, minutes;
+
+    public:
+
+    timeCalc ( int hrs = 0, int min = 0 ) {
+
+        hours = hrs, minutes = min ;
+    }
+
+    friend timeCalc processTime( timeCalc currDay, timeCalc nextDay ) ;
+
+    void displayTotalTime( void ) {
+
+        cout << "\nHours - "   << hours   ;
+        cout << "\nMinutes - " << minutes ;
+    }
+};
+
+
+timeCalc processTime ( timeCalc currDay, timeCalc nextDay ) {
+
+    timeCalc totalTime;
+
+    totalTime.hours   = currDay.hours   + nextDay.hours   ;
+    totalTime.minutes = currDay.minutes + nextDay.minutes ;
+
+    for ( ; totalTime.minutes >= 60; totalTime.minutes -= 60 ) {
+
+        totalTime.hours += 1;
+    }
+
+    return totalTime;
+}
+
+
+int main ( void ) {
+
+    timeCalc currDay ( 3, 69 ), nextDay ( 3, 167 );
+
+    timeCalc totalTime = processTime ( currDay, nextDay );
+
+    totalTime.displayTotalTime ();
+}
