c49caddca79c60d1348869550079a38c702a53a6
Shubham Mahajan <imahajanshubham@gmail.com>
2017-10-11 22:41:37 +0530

    LOLOL :D

--- vertHisto.c
+++ vertHisto.c
@@ -1,50 +1,39 @@
 
-	// -----An attempt to print Vertical Histogram----- //
+// -----An attempt to print Vertical Histogram----- //
 
-
-# include <stdio.h>
+#include <stdio.h>
 // #include <conio.h>
-
 
 // -----Extra variables used----- //
 int row = 0, colm = 0, j;
 char array[10][10], transpose[10][10];
 
+int main() {
+  int i;
 
-void main()
-{
-	int i;
+  while ((i = getchar()) != '\n') // EOF )
+  {
+    if (i == ' ' || i == '\t') {
+      // printf("\n");
+      colm = 0;
+      row++;
+    } else {
+      // printf("*");
+      array[row][colm] = '#';
+      colm++;
+    }
+  }
 
-	while( (i = getchar()) != '\n' ) // EOF )
-	{
-		if( i == ' '|| i == '\t' )
-		{
-			// printf("\n");
-			colm = 0;
-			row++;
-		}
-		else
-		{
-			// printf("*");
-			array[row][colm] = '#';
-			colm++;
-		}
-		
-	}
+  // -----Transpose of an Array----- //
+  for (i = 0; i < 10; ++i)
+    for (j = 0; j < 10; ++j)
+      transpose[i][j] = array[j][i];
 
+  // -----Reverse Display of an Array----- //
+  // -----$ Ignore how printf is written-- //
+  for (i = 9; i >= 0; i--)
+    for (j = 9; j >= 0; j--)
+      printf(j == 0 ? "%c\n" : "%c", transpose[i][j]);
 
-	// -----Transpose of an Array----- //	
-	for(     i = 0; i < 10; ++i )
-		for( j = 0; j < 10; ++j )
-    		transpose[i][j] = array[j][i];
-
-
-	// -----Reverse Display of an Array----- //
-	// -----$ Ignore how printf is written-- //
-	for(     i = 9; i >= 0; i-- )
-		for( j = 9; j >= 0; j-- )
-			printf( j == 0 ? "%c\n" : "%c", transpose[i][j] );
-
-
-	// getch();
+  return 0;
 }
